5349ef2844ac297afbe7797d1e5bdef129742759
Alvarezma <Derpachu@comcast.net>
2018-12-05 08:59:14 -0700

    Complete

---
+++ answers.md
@@ -0,0 +1,9 @@
+Questions-
+
+1. Yes the getPixels2D methos is mentioned several times but it isn't defined in the code.
+2. Yes but the getPixels2D method is only defined and not used in this file.
+3. No it doesnt compile instead I recieve an error message.
+4. Yes that code would compile just fine.
+5. Yes It should compile without problems.
+6. Yes the no-argument constructor compiles it just fine.
+7. No because a simple picture cannot be converted into a picture
--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,7 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
@@ -210,4 +211,47 @@ public class Picture extends SimplePicture {
 		beach.explore();
 	}
 
+	public void keepOnlyBlue() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+
+	public void negate() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setRed(255 - pixelObj.getRed());
+				pixelObj.setGreen(255 - pixelObj.getGreen());
+				pixelObj.setBlue(255 - pixelObj.getBlue());
+			}
+		}
+	}
+
+	public void grayscale() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				int average = (pixelObj.getRed() + pixelObj.getGreen() + pixelObj.getBlue()) / 3;
+				Color grayscale = new Color(average, average, average);
+				pixelObj.setColor(grayscale);
+			}
+		}
+	}
+
+	public void fixUnderwater() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setBlue(pixelObj.getBlue() + 20);
+				pixelObj.setGreen(pixelObj.getGreen() - 25);
+				pixelObj.setRed(pixelObj.getRed() + 50);
+
+			}
+		}
+	}
 } // this } is the end of class Picture, put all new methods before this
